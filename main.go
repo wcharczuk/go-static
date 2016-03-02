27cace4a03217861c2d57b7b8f557635e9b4d919
Will Charczuk <will@clotheshor.se>
2016-03-02 12:07:12 -0800

    things.

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	"github.com/blendlabs/go-util"
 )
+
+var portFlag string
 
 func port() string {
 	envPort := os.Getenv("PORT")
@@ -15,13 +21,68 @@ func port() string {
 	return "8080"
 }
 
-func main() {
+var pathFlag string
+
+func path() string {
+	if len(pathFlag) != 0 {
+		return pathFlag
+	}
+
+	return pwd()
+}
+
+func pwd() string {
 	pwd, pwdErr := os.Getwd()
 	if pwdErr != nil {
-		fmt.Errorf(pwdErr.Error())
+		err := fmt.Errorf(pwdErr.Error())
+		if err != nil {
+			log.Fatal(err)
+		}
 		os.Exit(1)
 	}
-	fmt.Printf("Static File Server Listening on: %s\n", port())
-	fs := http.FileServer(http.Dir(pwd))
-	log.Fatal(http.ListenAndServe(":"+port(), fs))
+	return pwd
+}
+
+func parseFlags() {
+	flag.StringVar(&portFlag, "port", "8080", "Port to listen on.")
+	flag.StringVar(&pathFlag, "path", "", "Path to serve.")
+	flag.Parse()
+}
+
+func main() {
+	parseFlags()
+
+	servePort := port()
+	servePath := path()
+
+	fmt.Printf("Static File Server Serving: `%s` Listening on: %s\n", servePath, servePort)
+
+	logger := log.New(os.Stdout, "", 0)
+
+	fs := FileServer(logger, http.Dir(servePath))
+	log.Fatal(http.ListenAndServe(":"+servePort, fs))
+}
+
+// FileServer returns a new (logged) FileHandler.
+func FileServer(log *log.Logger, root http.Dir) http.Handler {
+	return &FileHandler{Root: root, Handler: http.FileServer(root), Log: log}
+}
+
+// FileHandler is a logged version of http.fileHandler
+type FileHandler struct {
+	Root    http.Dir
+	Handler http.Handler
+	Log     *log.Logger
+}
+
+func (f *FileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now().UTC()
+	f.Handler.ServeHTTP(w, r)
+	end := time.Now().UTC()
+
+	ip := util.GetIP(r)
+	delta := end.Sub(start)
+	contentLength := w.Header().Get("Content-Length")
+
+	f.Log.Printf("%s - %s - %s - %s - %v - %s bytes", start.Format(time.RFC3339), ip, r.Method, r.URL.Path, delta, contentLength)
 }
